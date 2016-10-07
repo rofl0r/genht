2edf23c69d827935d538474605d89333b358a499
igor2 <igor2@localhost>
2016-10-07 17:45:30 +0100

    r47|-Fix: htsi has const char * key on read-only functions

--- src/htsi.h
+++ src/htsi.h
@@ -1,7 +1,9 @@
 #ifndef GENHT_HTSI_H
 #define GENHT_HTSI_H
 
+#define HT_HAS_CONST_KEY
 typedef char *htsi_key_t;
+typedef const char *htsi_const_key_t;
 typedef int htsi_value_t;
 #define HT(x) htsi_ ## x
 #include "ht.h"
