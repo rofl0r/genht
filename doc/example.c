3b1f7b9f0070150669285ecf3228c9218210e5f9
igor2 <igor2@localhost>
2021-07-19 05:54:54 +0100

    r111|[doc] -Fix: typo

--- doc/example.c
+++ doc/example.c
@@ -1,6 +1,6 @@
 /* genht string-to-integer example
 
-   Placed in the Public Domain (2021, Tibor 'Igor2' Palinkas
+   Placed in the Public Domain (2021, Tibor 'Igor2' Palinkas)
 
    This example reads stdin line by line and remembers the line number
    of the last occurance of each unique line content.
